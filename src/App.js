30af22a4a781aa078abd73720a91ddf0560d5644
Habon Assoweh <habonassoweh@Habons-Air>
2022-04-17 01:25:23 -0400

    Initialize a React Application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./App.css";
-import About from "./components/About";
+import Nav from "./components/Nav/Index";
+import About from "./components/About/Index";
+import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
